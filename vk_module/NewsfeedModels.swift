2ef1e02f6407e1b876feb6db81e9848b81572362
KIRILL896 <bka_inc@mail.ru>
2021-04-28 12:35:19 +0300

    Add files via upload

---
+++ vk_module/NewsfeedModels.swift
@@ -0,0 +1,63 @@
+//
+//  NewsfeedModels.swift
+//  VKNewsFeed
+//
+//  Created by отмеченные on 8.06.2020.
+//  Copyright © 2020 отмеченные. All rights reserved.
+//
+
+import UIKit
+
+enum Newsfeed {
+   
+  enum Model {
+    struct Request {
+      enum RequestType {
+        case getNewsfeed
+        case getUser
+        case revealPostIds(postId: Int)
+        case getNextBatch
+      }
+    }
+    struct Response {
+      enum ResponseType {
+        case presentNewsfeed(feed: FeedResponse, revealdedPostIds: [Int])
+        case presentUserInfo(user: UserResponse?)
+      }
+    }
+    struct ViewModel {
+      enum ViewModelData {
+        case displayNewsfeed(feedViewModel: FeedViewModel)
+        case displayUser(userViewModel: UserViewModel)
+      }
+    }
+  }
+}
+
+struct UserViewModel: TitleViewViewModel {
+    var photoUrlString: String?
+}
+
+struct FeedViewModel {
+    struct Cell: FeedCellViewModel {
+        var postId: Int
+        
+        var iconUrlString: String
+        var name: String
+        var date: String
+        var text: String?
+        var likes: String?
+        var comments: String?
+        var shares: String?
+        var views: String?
+        var photoAttachements: [FeedCellPhotoAttachementViewModel]
+        var sizes: FeedCellSizes
+    }
+    
+     struct FeedCellPhotoAttachment: FeedCellPhotoAttachementViewModel {
+        var photoUrlString: String?
+        var width: Int
+        var height: Int
+    }
+    let cells: [Cell]
+}
